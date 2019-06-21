ec931ff51382a674572de47ac6decae0d2b2e9fd
Aspire01 <18253137881@163.com>
2019-06-21 09:23:35 +0800

    123

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,15 @@
 import 'package:flutter/material.dart';
 import 'pages/tabs.dart';
 
-/**
- * 底部导航
- * 实现tabbar的切换
- */
 
+// 入口文件
 void main() => runApp(AppDemo());
 
 class AppDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Taps()
     );
   }
